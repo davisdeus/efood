808c8280c6b7776d6b3fd69bb96c627d21966786
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-10-23 22:02:27 -0300

    criando espasamento entre o header e a lista de restaurantes

--- src/components/RestauranteList/styles.ts
+++ src/components/RestauranteList/styles.ts
@@ -8,6 +8,7 @@ export const List = styled.div`
   grid-template-columns: 1fr 1fr;
   column-gap: 80px;
   row-gap: 48px;
+  margin-top: 50px;
 
   @media (max-width: ${breakpoints.desktop}) {
     column-gap: 130px;
